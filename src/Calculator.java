5972f1db1f3914a184ab461d646da77f22467213
golovachevavp <golovacheva.vp@gmail.com>
2022-09-16 23:10:22 +0300

    1

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,20 +3,19 @@ import java.util.List;
 
 public class Calculator {
     public static void main(String[] args) {
-        String str = "43-11+24*6/7";
+        String str = "3*11-7+24*6/7";
         System.out.println(calc(calc2(numbersToList(str))));
-
     }
 
     public static boolean isSign(String s) {
-        return (s.equals("+") || s.equals("-") || s.equals("*") ||s.equals("/") );
+        return (s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/"));
     }
 
-    public static boolean isSignPlusOrMinus(String s){
+    public static boolean isSignPlusOrMinus(String s) {
         return (s.equals("+") || s.equals("-"));
     }
 
-    public static boolean isSignMultiplicationOrDivision(String s){
+    public static boolean isSignMultiplicationOrDivision(String s) {
         return (s.equals("*") || s.equals("/"));
     }
 
@@ -32,62 +31,53 @@ public class Calculator {
             } else number = number.concat(str1[i]);
 
             if (i == str1.length - 1) numbers.add(number);
-
         }
         return numbers;
-
     }
 
     public static double calc(List<String> numbers) {
         double result = 0;
-        for (int i = 0; i < numbers.size() - 1; i++) {
-            if (i == 0) {
-                result = Double.parseDouble(numbers.get(i));
-            }
-
+        for (int i = 0; ; i++) {
             if (isSignPlusOrMinus(numbers.get(i))) {
-                result = actionSumOrSubtraction(numbers.get(i), result, Double.parseDouble(numbers.get(i + 1)));
+                result = actionSumOrSubtraction(numbers.get(i), Double.parseDouble(numbers.get(i - 1)), Double.parseDouble(numbers.get(i + 1)));
+                numbers.set(i - 1, String.valueOf(result));
+                numbers.remove(i);
+                numbers.remove(i);
+                i = i - 1;
             }
+            if (i == numbers.size() - 1) break;
         }
         return result;
-
     }
 
     public static List<String> calc2(List<String> numbers) {
         double result = 0;
-        for (int i = 0; ;i++) {
-        if (isSignMultiplicationOrDivision(numbers.get(i))) {
+        for (int i = 0; ; i++) {
+            if (isSignMultiplicationOrDivision(numbers.get(i))) {
                 result = actionMultiplicationOrDivision(numbers.get(i), Double.parseDouble(numbers.get(i - 1)), Double.parseDouble(numbers.get(i + 1)));
-                numbers.set(i-1, String.valueOf(result));
+                numbers.set(i - 1, String.valueOf(result));
                 numbers.remove(i);
                 numbers.remove(i);
-                i=i-1;
+                i = i - 1;
             }
-         if (i == numbers.size() - 1) break;
+            if (i == numbers.size() - 1) break;
         }
         return numbers;
-
     }
-    public static double actionSumOrSubtraction(String sign, double a, double b) {
 
+    public static double actionSumOrSubtraction(String sign, double a, double b) {
         return switch (sign) {
             case "+" -> a + b;
             case "-" -> a - b;
             default -> 0;
         };
-
     }
-    public static double actionMultiplicationOrDivision(String sign, double a, double b) {
 
+    public static double actionMultiplicationOrDivision(String sign, double a, double b) {
         return switch (sign) {
             case "*" -> a * b;
             case "/" -> a / b;
             default -> 0;
         };
-
     }
-
-
-
-
 }
